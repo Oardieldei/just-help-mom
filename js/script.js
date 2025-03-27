72a70e0f82cd26f65202afa7b404a39feb295c5c
Alik sexy <alik.want.you@gmail.com>
2025-03-27 11:41:18 +0300

    fix: fix calendar

--- js/script.js
+++ js/script.js
@@ -4,12 +4,18 @@ const calendarDates = document.querySelector('.calendar__dates')
 const getNumberOfFisrtDay = () => {
 	const chosenDate = new Date(dataObjectToUse.year, dataObjectToUse.month)
 	const firstDay = chosenDate.getDay()
-	return firstDay
+	return firstDay === 0 ? 7 : firstDay
 }
 
 const getLastDay = () => {
 	let lastDay = new Date(dataObjectToUse.year, dataObjectToUse.month + 1, 0)
 	return lastDay.getDate()
+}
+
+const getNumberOfLastDay = () => {
+	const chosenDate = new Date(dataObjectToUse.year, dataObjectToUse.month + 1, 0)
+	const lastDay = chosenDate.getDay()
+	return lastDay === 0 ? 7 : lastDay
 }
 
 const createEmptyCellForDay = () => {
@@ -19,9 +25,11 @@ const createEmptyCellForDay = () => {
 	calendarDates.append(newCell)
 }
 
-const createCellForDay = () => {
+const createCellForDay = (dateNum) => {
 	const newCell = document.createElement('div')
 	newCell.classList.add('calendar__dates_cell')
+	newCell.innerText = dateNum
+	newCell.id = `day${dateNum}`
 	calendarDates.append(newCell)
 }
 
@@ -33,13 +41,22 @@ const createEmptyCellsBefore = () => {
 
 const createCells = () => {
 	for (let i = 0; i < getLastDay(); i++) {
-		createCellForDay()
+		createCellForDay(i + 1)
+	}
+}
+
+const createEmptyCellsAfter = () => {
+	for (let i = 0; i < 7 - getNumberOfLastDay(); i++) {
+		createEmptyCellForDay()
 	}
 }
 
 const createCalendar = () => {
 	createEmptyCellsBefore()
 	createCells()
+	createEmptyCellsAfter()
 }
 
 createCalendar()
+
+console.log(getNumberOfFisrtDay(), getNumberOfLastDay())
